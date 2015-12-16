3acc62f242aca79977820679e7ea679cb52bc7a8
Erol Akarsu <eakarsu@erols-mac.local>
2015-12-16 18:40:07 -0500

    Initial Commit

---
+++ Fav-movies/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fav-movies
+//
+//  Created by Erol Akarsu on 12/16/15.
+//  Copyright © 2015 Erol Akarsu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
